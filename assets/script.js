8b5f1f88fde6488189e28ab8d79c5bec823c5fba
Rithyreach <ritty690@gmail.com>
2024-11-17 17:40:20 +0700

    four commit

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,11 @@ let iconToggler = document.querySelector("#menuTogger");
 let navList = document.querySelector(".navlist");
 
 iconToggler.addEventListener("click", () => {
-    iconToggler.classList.toggle("fa-xmark");
-    navList.classList.toggle("active");
+  iconToggler.classList.toggle("fa-xmark");
+  navList.classList.toggle("active");
 });
 
 navList.addEventListener("click", () => {
-    iconToggler.classList.remove("fa-xmark");
-    navList.classList.toggle("active");
-
-})
+  iconToggler.classList.remove("fa-xmark");
+  navList.classList.toggle("active");
+});
